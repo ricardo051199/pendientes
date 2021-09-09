aa5db2d13d7e2088a9e1fee9f417214ab95b1a4c
ricardo pelaez <jesus.pelaez@ucb.edu.bo>
2021-09-09 09:03:46 -0400

    tercera version

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ const form = document.querySelector("#todo-form");
 
 const todo = document.querySelector("#todo-input");
 const work = document.querySelector("#work-input");
+const filter = document.querySelector("#filter-input");
 
 form.addEventListener("submit", (event) => {
-    alert("Todo : " + todo.value + "  " + "Work : " + work.value);
+    alert("Todo : " + todo.value + "  " + "Work : " + work.value+ "  " + "Filter : " + filter.value);
 });
